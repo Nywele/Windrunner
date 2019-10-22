73ec60391f5f4f823b7768b74311b2b086650a45
Weiyanyu <chinayeonon@gmail.com>
2019-10-22 22:41:12 +0800

    init project

---
+++ src/main/java/top/yeonon/windrunner/WindrunnerApplication.java
@@ -0,0 +1,13 @@
+package top.yeonon.windrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindrunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindrunnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/top/yeonon/windrunner/WindrunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package top.yeonon.windrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindrunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
